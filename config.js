bc8df3111f57f5739807b24b85cda37bf6fb0038
manishtmtmt <manisht.mt.mt@gmail.com>
2024-01-11 17:17:00 +0530

    changed mongo uri

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-require("dotenv").config();
 const { connect } = require("mongoose");
-
-const mongodb_uri = process.env.MONGODB_URI || "mongodb://localhost:27017/meet";
 
 const connection = connect(
   "mongodb+srv://manishtmtmt:admin@cluster0.dhuzq3p.mongodb.net/video-meet?retryWrites=true&w=majority"
