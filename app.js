ec0ff3120561e1678bb9c38cacbaca6eb2d8e653
manishtmtmt <manisht.mt.mt@gmail.com>
2024-01-17 17:09:54 +0530

    added cors

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ const authRouter = require("./router/auth.router");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
