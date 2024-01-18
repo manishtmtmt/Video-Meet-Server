b3d67b6ed08dabcaed2e208e7c68d7827545b8bd
manishtmtmt <manisht.mt.mt@gmail.com>
2024-01-18 13:12:32 +0530

    changed config

--- socket/video-conference.js
+++ socket/video-conference.js
@@ -470,7 +470,7 @@ function socketMain(io) {
         {
           protocol: "udp",
           ip: "127.0.0.1",
-          announcedIp: "13.232.240.57",
+          announcedIp: "127.0.0.1",
         },
       ],
       enableUdp: true,
